1a48329cf034f5f197c2de964559b25ed5822a99
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-10 23:30:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/teo/a21startactivityforresult/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.teo.a21startactivityforresult;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/teo/a21startactivityforresult/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.teo.a21startactivityforresult;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+
+    }
+}
