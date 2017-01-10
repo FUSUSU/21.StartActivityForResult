0e31c7645bcf4ab7d12820da03e6f33a13b7bb48
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-11 00:12:22 +0700

    Hoàn thành activitymain chuyển qua được secondmain và có thể kiểm tra mã thông qua

--- app/src/main/java/com/example/teo/a21startactivityforresult/MainActivity.java
+++ app/src/main/java/com/example/teo/a21startactivityforresult/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.teo.a21startactivityforresult;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
         //Ánh xạ
         textView1 = (TextView) findViewById(R.id.textView1);
         button1 = (Button) findViewById(R.id.button1);
+
+        //Bắt sự kiện click button
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 2);
+            }
+        });
+    }
+
+    // Call back method to get the Message form other activity
+    //Gọi lại phương thức để có được những tin nhắn dạng hoạt động khác
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Check if the request code is same as what is passed here it is 2
+        //Kiểm tra xem mã yêu cầu có giống như những gì thông ở đây là 2 không?
+        if (requestCode == 2){
+            String message = data.getStringExtra("MESSAGE");
+            textView1.setText(message);
+        }
     }
 
     //Menu item
