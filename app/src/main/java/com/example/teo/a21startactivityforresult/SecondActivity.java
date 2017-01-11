293e506f2238b3facebfda8ba64a97b9b0ab8af9
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-11 10:38:21 +0700

    Menu item, ánh xạ, bắt sự kiện của SecondActivity.java

--- app/src/main/java/com/example/teo/a21startactivityforresult/SecondActivity.java
+++ app/src/main/java/com/example/teo/a21startactivityforresult/SecondActivity.java
@@ -2,13 +2,35 @@ package com.example.teo.a21startactivityforresult;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+    EditText editText1;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_main);
 
+        //Ánh xạ
+        editText1 = (EditText) findViewById(R.id.editText01);
+        button1 = (Button) findViewById(R.id.button01);
+        //Bắt sự kiện
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    //Menu item
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
